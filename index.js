b008701083f2f286b22a52635a72317ed3391c29
Juan Pablo <juanpospinagualdron@gmail.com>
2023-12-08 21:28:20 -0500

    Adición de conversor Hex a RGB y finalización del proyecto

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 console.log('Conectando...')
 
+// Conversor RGB a Hexadecimal
+
 const r = document.getElementById('R')
 const g = document.getElementById('G')
 const b = document.getElementById('B')
@@ -26,6 +28,31 @@ function mostrar(){
 
     RTA.style =  "color:black"
     RTA.innerText = "El número RGB " + '('+rgb+')' + " en hexadecimal es " + hex;
+}
+
+// Conversor Hexadecimal a RGB
+
+const num = document.getElementById('hex')
+const calHex = document.getElementById('btnHex')
+const rta_hex = document.getElementById('rtaHex')
+
+calHex.addEventListener('click', hexToRgb)
+
+function hexToRgb(hex) {
+
+    const hexNum = num.value
+
+    // Eliminar el signo # si está presente
+    hex = hexNum.replace(/^#/, '');
+  
+    // Convertir a valores RGB
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+
+    rta_hex.style = "color:black"
+    rta_hex.innerText = "El número Hexadecimal " + '('+'#'+hexNum+')' + " en RGB es " + '(' + r +', '+ g +', '+ b +')'
+    
 }
 
 console.log('Conectado.')
